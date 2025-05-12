5058ec994cde35971efa2bfecfc744ae6101a2bf
mariisbx <mariaaparecida.ifc.araquari@gmail.com>
2025-05-11 21:07:30 -0300

    FEAT: Criação e formatação das categorias #3

--- src/App.vue
+++ src/App.vue
@@ -82,6 +82,22 @@ let listaLivros = ref([
         <div><img class="banner" :src="livroSelecionado.capa" alt="foto"></div>
       </div>
       </section>
+       <section class="categorias">
+        <ul>
+          <li class="border">
+            <span id="truck" class="fa-solid fa-truck-fast"></span>
+            <p>Frete Grátis para o litoral</p>
+          </li>
+          <li>
+            <span id="star" class="fa-solid fa-star"></span>
+            <p>Mais vendidos</p>
+          </li>
+          <li class="borderb">
+            <span id="livros" class="fa-solid fa-book-open"></span>
+            <p>Livros recomendados</p>
+          </li>
+        </ul>
+      </section>
     </main>
     </body>
 </template>
@@ -191,5 +207,42 @@ img {
 }
 .exibicao div.texto p.sinapse {
   font-size: 1.2rem;
+}
+.categorias {
+  margin: 4vw 0 0 0;
+  border-bottom: 2px solid #FF5722;
+  padding: 0 0 5vw 0;
+}
+.categorias ul {
+  display: flex;
+}
+.categorias ul li {
+  margin: 2vw 8vw 2vw 5vw;
+}
+.categorias ul span#star {
+  margin: 0 10px 8px 52px;
+}
+.categorias ul span#truck {
+  margin: 0 10px 8px 106px;
+}
+.categorias ul span#livros {
+  margin: 0 10px 8px 83px;
+}
+.categorias ul span {
+  font-size: 2rem;
+  color: #FF5722;
+}
+.categorias ul li p {
+  font-size: 1.3rem;
+  width: 130%;
+  font-weight: 700;
+}
+.categorias ul li.border {
+  border-right: #FF5722 solid 3px;
+  padding: 0 7vw;
+}
+.categorias ul li.borderb {
+  border-left: #FF5722 solid 3px;
+  padding: 0 7vw;
 }
 </style>
