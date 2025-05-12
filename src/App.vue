285d9b0d598bec706b0cf44162324655c4d90b2e
mariisbx <mariaaparecida.ifc.araquari@gmail.com>
2025-05-11 21:15:52 -0300

    FEAT: Criação e formatação da loja #4

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,18 @@ let listaLivros = ref([
 
   let livroSelecionado = ref(sorteando());
 
+   let carrinho = ref([]);
+  function adicionarCarrinho(livro) {
+  const existente = carrinho.value.find(item => item.id === livro.id);
+
+  if (existente) {
+    existente.quantidade++;
+  } else {
+    livro.adicionado = true;
+    livro.quantidade = 1;
+    carrinho.value.push(livro);
+  }
+}
 </script>
 
 <template>
@@ -95,6 +107,26 @@ let listaLivros = ref([
           <li class="borderb">
             <span id="livros" class="fa-solid fa-book-open"></span>
             <p>Livros recomendados</p>
+          </li>
+        </ul>
+      </section>
+      <section class="loja">
+         <h2>Livros Disponíveis:</h2>
+        <ul>
+          <li v-for="livro in listaLivros" :key="livro.id">
+            <img :src="livro.capa" alt="capa">
+            <h3>{{ livro.nome }}</h3>
+            <p>{{ livro.autor }}
+              <span class="fa-regular fa-heart"></span>
+            </p>
+            <p>{{ livro.preco }}</p>
+            <button
+          v-if="!livro.adicionado"
+          @click="adicionarCarrinho(livro)"
+        >
+          <span class="fa-solid fa-cart-shopping"></span>
+        </button>
+        <button v-else>Adicionado</button>
           </li>
         </ul>
       </section>
@@ -244,5 +276,42 @@ img {
 .categorias ul li.borderb {
   border-left: #FF5722 solid 3px;
   padding: 0 7vw;
+}
+.loja ul {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 3vw 0 7.5vw ;
+  justify-content: center;
+  padding: 0;
+}
+.loja ul li {
+  margin: 6vw 1.5vw 3vw 1.5vw;
+  width: 320px;
+  margin: 2vw;
+}
+.loja h3 {
+  font-family: "Patua One", serif;
+  margin: 20px 0 0 0 ;
+  font-size: 1rem;
+}
+.loja p {
+  font-size: 1.1rem;
+  margin: 10px 0 10px 0;
+}
+.loja button {
+  font-size: 1.2rem;
+  padding: 10px 100px 10px 100px;
+  color: #FF5722;
+  background-color: white;
+  border: 2px solid #FF5722;
+  margin: 10px 0 0 0;
+  margin: 1vw 0 0 3px;
+}
+.loja h2 {
+  font-size: 1.8rem;
+  margin: 5vw 6vw 3vw 6vw;
+  border-bottom: 2px solid #FF5722;
+  width: 14%;
+  padding: 0 0 10px 0;
 }
 </style>
